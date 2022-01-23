0cd20ec71c78e247da9586fe2bc90140691b2e8d
jlesly <leslyj2@gmail.com>
2022-01-23 11:48:12 -0500

    commit tree b610fea62f53f8737ba9c43f3a20281d27b7b9cd

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux'
-import rootReducer from './reducers/index'
+import { combineReducers } from "redux";
+import quotes from './quotes';
 
-let store = createStore(rootReducer)
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+export default combineReducers({
+  quotes,
+});
